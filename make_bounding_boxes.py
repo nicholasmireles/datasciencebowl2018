4c6277609989cc543ced568f1701e27b90a598e2
Stephen Melsom <stephen.melsom@noblis.org>
2018-08-15 20:02:03 -0400

    Added frcnn submodule and updated bounding box file.

--- make_bounding_boxes.py
+++ make_bounding_boxes.py
@@ -10,15 +10,17 @@ f.close()
 
 bbs = []
 train_dict = json.loads(f_raw)
-for im, seg_ims in tqdm(train_dict.iteritems(), total=len(train_dict.keys())):
+for im, seg_ims in tqdm(train_dict.items(), total=len(train_dict.keys())):
 	for seg_im in seg_ims:
 		img = imread(seg_im)
 		top = np.argwhere(img==255)[:,0].min()
 		bottom = np.argwhere(img==255)[:,0].max()
 		left = np.argwhere(img==255)[:,1].min()
 		right = np.argwhere(img==255)[:,1].min()
-		bbs.append([im, seg_im, top, left, bottom, right])
+		#bbs.append([im, seg_im, left, top, right, bottom, 'nucleus'])
+		bbs.append([im, left, top, right, bottom, 'nucleus'])
 
 df = pd.DataFrame().from_dict(bbs)
-df.columns = ['image', 'mask', 'top', 'left', 'bottom', 'right']
+#df.columns = ['image', 'mask', 'left', 'top', 'right', 'bottom', 'class']
+df.columns = ['image', 'left', 'top', 'right', 'bottom', 'class']
 df.to_csv('mask_bounding_boxes.csv', index=False)
