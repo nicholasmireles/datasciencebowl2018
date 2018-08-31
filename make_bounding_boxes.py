bd047c1dd7ba24d0c7a49d96bcea3a453d1c52f4
Stephen Melsom <stephen.melsom@noblis.org>
2018-08-31 12:19:00 -0400

    Added stuff

--- make_bounding_boxes.py
+++ make_bounding_boxes.py
@@ -13,18 +13,18 @@ cwd = os.getcwd()
 
 bbs = []
 train_dict = json.loads(f_raw)
-for im, seg_ims in tqdm(train_dict.items(), total=len(train_dict.keys())):
+for im, seg_ims in tqdm(train_dict.items()):
     for seg_im in seg_ims:
         img = imread(seg_im)
         top = np.argwhere(img==255)[:,0].min()
         bottom = np.argwhere(img==255)[:,0].max()
         left = np.argwhere(img==255)[:,1].min()
-        right = np.argwhere(img==255)[:,1].min()
-        #bbs.append([im, seg_im, left, top, right, bottom, 'nucleus'])
+        right = np.argwhere(img==255)[:,1].max()
+        if top >= bottom or left >= right:
+            continue
         im = os.path.join(cwd, im)
-        bbs.append([im, left, top, right, bottom, 'nucleus'])
+        seg_im = os.path.join(cwd, seg_im)
+        bbs.append([im, left, top, right, bottom, 'nucleus', seg_im])
 
 df = pd.DataFrame().from_dict(bbs)
-#df.columns = ['image', 'mask', 'left', 'top', 'right', 'bottom', 'class']
-df.columns = ['image', 'left', 'top', 'right', 'bottom', 'class']
 df.to_csv('mask_bounding_boxes.csv', index=False, header=False)
